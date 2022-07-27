1a086eaf28a34f455a285c05792fe183ca42cadf
facundo <sainifacunddo@gmail.com>
2022-07-27 10:41:20 -0300

    agregar categoria

--- src/GifsApp.jsx
+++ src/GifsApp.jsx
@@ -5,10 +5,15 @@ export const GifsApp = () => {
 
   const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
 
+  const onAddCat = () => {
+    setCategories(["Mob 100", ...categories]);
+  };
+
   console.log("categories = ", categories);
   return (
     <>
       <h1>Gifs App</h1>
+      <button onClick={onAddCat}>Agregar</button>
       <ol>
         {categories.map((c) => (
           <li key={c}>{c}</li>
