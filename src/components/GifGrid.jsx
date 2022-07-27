88ead9cef5dd344ffeaef35043d896fefbbea0cd
facundo <sainifacunddo@gmail.com>
2022-07-27 12:46:32 -0300

    useEffect

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,8 +1,10 @@
+import { useEffect } from "react";
 import { getGifs } from "../helpers/getGifs";
 
 export const GifGrid = ({ cat }) => {
-  const gifs = getGifs(cat);
-
+  useEffect(() => {
+    getGifs(cat);
+  }, []);
   return (
     <>
       <h3>{cat}</h3>
