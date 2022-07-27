ff0b7114e79238f03653aa008b0716b59314ba48
facundo <sainifacunddo@gmail.com>
2022-07-27 10:31:52 -0300

    lista de categorias

--- src/GifsApp.jsx
+++ src/GifsApp.jsx
@@ -1,8 +1,19 @@
+import { useState } from "react";
+
 export const GifsApp = () => {
   const apiKey = "cv8V5riGJxpNnkhGzKz86NcOX8HwNdeg";
+
+  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+
+  console.log("categories = ", categories);
   return (
     <>
       <h1>Gifs App</h1>
+      <ol>
+        {categories.map((c) => (
+          <li key={c}>{c}</li>
+        ))}
+      </ol>
     </>
   );
 };
