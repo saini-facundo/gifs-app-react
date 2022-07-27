78147fd05591baa104a9021e2b53ad246d4b6f43
facundo <sainifacunddo@gmail.com>
2022-07-27 10:09:26 -0300

    init

---
+++ src/GifsApp.jsx
@@ -0,0 +1,8 @@
+export const GifsApp = () => {
+  const apiKey = "cv8V5riGJxpNnkhGzKz86NcOX8HwNdeg";
+  return (
+    <>
+      <h1>Gifs App</h1>
+    </>
+  );
+};
---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { GifsApp } from "./GifsApp";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <GifsApp />
+  </React.StrictMode>
+);
