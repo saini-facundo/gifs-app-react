bbc6d97fa27f984562f81b98491390ce53bde00f
facundo <sainifacunddo@gmail.com>
2022-07-28 15:15:19 -0300

    mock customHooks

--- tests/components/GifGrid.test.jsx
+++ tests/components/GifGrid.test.jsx
@@ -1,14 +1,40 @@
 import { render, screen } from "@testing-library/react";
 import { GifGrid } from "../../src/components";
+import { useFetchGifs } from "../../src/hooks/useFetchGifs";
+
+jest.mock("../../src/hooks/useFetchGifs");
 
 describe("Pruebas en <GifGrid/>", () => {
   const testCat = "One Punch";
 
-  test("debe mostar Loading inicialmente", () => {
+  test("debe mostrar Loading inicialmente", () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
     render(<GifGrid cat={testCat} />);
-    //screen.debug();
-
     expect(screen.getByText("Cargando...")).toBeTruthy();
     expect(screen.getByText(testCat)).toBeTruthy();
+  });
+
+  test("debe mostar items cuando se cargan las imagenes con useFetchGifs", () => {
+    const gifs = [
+      {
+        id: "abc",
+        title: "Saitama",
+        url: "https://localhost/saitama.jpg",
+      },
+      {
+        id: "sdf556",
+        title: "Goku",
+        url: "https://localhost/goku.jpg",
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: true,
+    });
+    render(<GifGrid cat={testCat} />);
+    expect(screen.getAllByRole("img").length).toBe(2);
   });
 });
